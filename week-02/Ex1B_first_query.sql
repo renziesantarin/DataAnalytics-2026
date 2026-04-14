c377148f973b7e34572340a42992370dcf9d9178
Marenza Santarin <renziesantarin@gmail.com>
2026-04-14 11:31:41 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,9 @@
 /*
 Sakila is a database used to collect information about films, inventory, and rentals for use by a movie rental store and its employees.
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- Retrieved 1000 records
+
